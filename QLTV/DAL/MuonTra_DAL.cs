f75907606d49a1eb2005028f30f1334657f0349b
baoht1 <baoht@media-one.vn>
2020-06-24 20:59:11 +0700

    muon tra dal

---
+++ QLTV/DAL/MuonTra_DAL.cs
@@ -0,0 +1,146 @@
+﻿using QLTV.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using static QLTV.DTO.MuonTra;
+
+namespace QLTV.DAL
+{
+    class MuonTra_DAL
+    {
+
+        private static MuonTra_DAL instance;
+        public static MuonTra_DAL Instance
+        {
+            get { if (instance == null) instance = new MuonTra_DAL(); return MuonTra_DAL.instance; }
+            private set { MuonTra_DAL.instance = value; }
+        }
+
+        private MuonTra_DAL()
+        {
+        }
+        // Dưới đây là các chức năng conn DB
+        // Truy vấn
+       //-- PhieuMuon
+        public List<PhieuMuon>GetListPhieuMuon()
+        {
+            List<PhieuMuon> list = new List<PhieuMuon>();
+
+            string query = "select * from PhieuMuon_View";
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach( DataRow item in data.Rows)
+            {
+                PhieuMuon phieumuon = new PhieuMuon(item);
+                list.Add(phieumuon);
+            }
+            return list;
+;        }
+        // -- PhieuTra
+        public List<PhieuTra> GetListPhieuTra()
+        {
+            List<PhieuTra> list = new List<PhieuTra>();
+
+            string query = "select * from PhieuTra_View";
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                PhieuTra phieutra = new PhieuTra(item);
+                list.Add(phieutra);
+            }
+            return list;
+            ;
+        }
+        //--Tìm kiếm Phiếu Mượn
+        // By Ma The
+        public List<PhieuMuon> SearchPhieuMuonByTheID(int id)
+        {
+
+            List<PhieuMuon> list = new List<PhieuMuon>();
+
+            string query = string.Format($"EXEC TimPhieuMuon '{id}','','0'");
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                PhieuMuon phieumuon = new PhieuMuon(item);
+                list.Add(phieumuon);
+            }
+
+            return list;
+        }
+        // By Ngay Muon
+        public List<PhieuMuon> SearchPhieuMuonByNgayMuon(DateTime ngaymuon)
+        {
+
+            List<PhieuMuon> list = new List<PhieuMuon>();
+
+            string query = string.Format($"EXEC TimPhieuMuon '','{ngaymuon}','1'");
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                PhieuMuon phieumuon = new PhieuMuon(item);
+                list.Add(phieumuon);
+            }
+
+            return list;
+        }
+        //Phi truy vấn
+        public bool UpdatePhieuMuon(int maPhieuMuon, int maThe, DateTime ngayMuon, DateTime ngayHanTra, int maCuonSach, int maNhanVien, int maCuonSachNew)
+        {
+            string query = string.Format($"EXEC SuaPhieuMuon '{maPhieuMuon}','{ngayMuon}', '{ngayHanTra}','{maThe}', '{maCuonSach}', '{maNhanVien}', {maCuonSachNew}");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+
+        public bool InsertPhieuMuon(int maThe, DateTime ngayMuon, DateTime ngayHanTra, int maCuonSach, int maNhanVien)
+        {
+            string query = string.Format($"EXEC ThemPhieuMuon '{ngayMuon}', '{ngayHanTra}','{maThe}', '{maCuonSach}', '{maNhanVien}'");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+
+        public bool DeletePhieuMuon(int maPhieuMuon)
+        {
+            string query = string.Format($"EXEC XoaPhieuMuon {maPhieuMuon}");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+
+        public bool UpdatePhieuTra(int maPhieuTra, int maThe, DateTime ngayTra,int maCuonSach, int maNhanVien, int maCuonSachNew)
+        {
+            string query = string.Format($"EXEC SuaPhieuTra '{maPhieuTra}', '{ngayTra}','{maThe}', '{maCuonSach}', '{maNhanVien}', '{maCuonSachNew}'");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+
+        public bool InsertPhieuTra(int maThe, DateTime ngayTra, int maCuonSach, int maNhanVien)
+        {
+            string query = string.Format($"EXEC ThemPhieuTra '{ngayTra}','{maThe}', '{maCuonSach}', '{maNhanVien}'");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+
+        public bool DeletePhieuTra(int maPhieuTra)
+        {
+            string query = string.Format($"EXEC XoaPhieuTra {maPhieuTra}");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+    }
+}
---
+++ QLTV/DAL/Sach_DAL.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLTV.DTO;
+namespace QLTV.DAL
+{
+    public class Sach_DAL
+    {
+        private static Sach_DAL instance;
+
+        public static Sach_DAL Instance
+        {
+            get { if (instance == null) instance = new Sach_DAL(); return Sach_DAL.instance; }
+            private set { Sach_DAL.instance = value; }
+        }
+        public List<TacGia> GetListTacGia()
+        {
+            List<TacGia> list = new List<TacGia>();
+
+            string query = "select * from TACGIA_View";
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                TacGia tacGia = new TacGia(item);
+                list.Add(tacGia);
+            }
+
+            return list;
+        }
+        public List<NXB> GetListNXB()
+        {
+            List<NXB> list = new List<NXB>();
+
+            string query = "select * from NXB_View";
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                NXB nxb = new NXB(item);
+                list.Add(nxb);
+            }
+
+            return list;
+        }
+        public List<Sach> GetListCuonSach()
+        {
+            List<Sach> list = new List<Sach>();
+
+            string query = "select * from CUONSACH_View";
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                Sach cuonsach = new Sach(item);
+                list.Add(cuonsach);
+            }
+
+            return list;
+        }
+        public List<DauSach> GetListDauSach()
+        {
+            List<DauSach> list = new List<DauSach>();
+
+            string query = "select * from DAUSACH_View";
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                DauSach dausach = new DauSach(item);
+                list.Add(dausach);
+            }
+
+            return list;
+        }
+        public List<TheLoai> GetListTheLoai()
+        {
+            List<TheLoai> list = new List<TheLoai>();
+
+            string query = "select * from THELOAI_View";
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                TheLoai theLoai = new TheLoai(item);
+                list.Add(theLoai);
+            }
+
+            return list;
+        }
+        public List<TacGia> SearchTacGiaByID(int id)
+        {
+
+            List<TacGia> list = new List<TacGia>();
+
+            string query = string.Format($"SELECT * FROM TimKiemTG1('{id}')");
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                TacGia tacGia = new TacGia(item);
+                list.Add(tacGia);
+            }
+
+            return list;
+        }
+        public List<TacGia> SearchTacGiaByName(string name)
+        {
+
+            List<TacGia> list = new List<TacGia>();
+
+            string query = string.Format($"SELECT * FROM TimKiemTG2(N'{name}')");
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                TacGia tacGia = new TacGia(item);
+                list.Add(tacGia);
+            }
+
+            return list;
+        }
+        public List<NXB> SearchNXBByID(int id)
+        {
+
+            List<NXB> list = new List<NXB>();
+
+            string query = string.Format($"SELECT * FROM TimKiemNXB1('{id}')");
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                NXB nxb = new NXB(item);
+                list.Add(nxb);
+            }
+
+            return list;
+        }
+        public List<NXB> SearchNXBByName(string name)
+        {
+
+            List<NXB> list = new List<NXB>();
+
+            string query = string.Format($"SELECT * FROM TimKiemNXB2(N'{name}')");
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                NXB nxb = new NXB(item);
+                list.Add(nxb);
+            }
+
+            return list;
+        }
+        public List<Sach> SearchSachByID(int id)
+        {
+
+            List<Sach> list = new List<Sach>();
+
+            string query = string.Format($"SELECT * FROM TimKiemCS1('{id}')");
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                Sach cuonsach = new Sach(item);
+                list.Add(cuonsach);
+            }
+
+            return list;
+        }
+        // Hưng thay tạm
+        public List<Sach> SearchSachByID1(int id)
+        {
+
+            List<Sach> list = new List<Sach>();
+
+            string query = string.Format($"EXEC TimKiemCS '{id}'");
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                Sach cuonsach = new Sach(item);
+                list.Add(cuonsach);
+            }
+
+            return list;
+        }
+
+        public List<Sach> SearchSachByName(string name)
+        {
+
+            List<Sach> list = new List<Sach>();
+
+            string query = string.Format($"SELECT * FROM TimKiemCS2(N'{name}')");
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                Sach cuonsach = new Sach(item);
+                list.Add(cuonsach);
+            }
+
+            return list;
+        }
+        public List<DauSach> SearchDauSachByID(int id)
+        {
+
+            List<DauSach> list = new List<DauSach>();
+
+            string query = string.Format($"SELECT * FROM TimKiemDS1('{id}')");
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                DauSach dausach = new DauSach(item);
+                list.Add(dausach);
+            }
+
+            return list;
+        }
+        public List<DauSach> SearchDauSachByName(string name)
+        {
+
+            List<DauSach> list = new List<DauSach>();
+
+            string query = string.Format($"SELECT * FROM TimKiemDS2(N'{name}')");
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                DauSach dausach = new DauSach(item);
+                list.Add(dausach);
+            }
+
+            return list;
+        }
+        public List<TheLoai> SearchTheLoaiByName(string name)
+        {
+
+            List<TheLoai> list = new List<TheLoai>();
+
+            string query = string.Format($"SELECT * FROM TimKiemTheLoai(N'{name}')");
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                TheLoai theloai = new TheLoai(item);
+                list.Add(theloai);
+            }
+
+            return list;
+        }
+        public bool InsertTacGia(int MaTacGia,string TenTG)
+        {
+            string query = string.Format($"EXEC ThemTacGia '{MaTacGia}', [{TenTG}]");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+        public bool InsertNXB(int MaNXB, string TenNXB,string diachiNXB,int sdt)
+        {
+            string query = string.Format($"EXEC ThemNXB '{MaNXB}', [{TenNXB}],[{diachiNXB}],'{sdt}'");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+        public bool InsertCuonSach(int MaCuonSach, string TenSach, int page, string tinhtrang,int madausach,int makesach)
+        {
+            string query = string.Format($"EXEC themCuonSach '{MaCuonSach}', [{TenSach}],'{page}',[{tinhtrang}],'{madausach}','{makesach}'");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+        public bool InsertDauSach(int MaDauSach, string TenDauSach, int MaNXB)
+        {
+            string query = string.Format($"EXEC themDauSach '{MaDauSach}', [{TenDauSach}],'{MaNXB}'");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+        public bool InsertTheLoai(int MaKeSach, string TenTheLoai)
+        {
+            string query = string.Format($"EXEC themTheLoai '{MaKeSach}', [{TenTheLoai}]");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+        public bool DeleteTacGia(int MaTG)
+        {
+            string query = string.Format($"EXEC xoaTG '{MaTG}'");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+            return result > 0;
+   
+        }
+        public bool DeleteNXB(int MaNXB)
+        {
+            string query = string.Format($"EXEC deleteNXB '{MaNXB}'");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+            return result > 0;
+
+        }
+        public bool DeleteTheLoai(int MaKeSach)
+        {
+            string query = string.Format($"EXEC deleteTheLoai '{MaKeSach}'");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+            return result > 0;
+
+        }
+        public bool DeleteCuonSach(int MaCS)
+        {
+            string query = string.Format($"EXEC deleteCuonSach '{MaCS}'");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+            return result > 0;
+
+        }
+        public bool DeleteDauSach(int MaDS)
+        {
+            string query = string.Format($"EXEC deleteDausach '{MaDS}'");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+            return result > 0;
+
+        }
+        public bool UpdateTacGia(int MaTG,string TenTG)
+        {
+            string query = string.Format($"EXEC updateTG '{MaTG}',[{TenTG}]");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+        public bool UpdateNXB(int MaNXB, string TenNXB,string diachiNXB,int sdt)
+        {
+            string query = string.Format($"EXEC updateNXB '{MaNXB}',[{TenNXB}],[{diachiNXB}],'{sdt}'");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+        public bool UpdateCuonSach(int MaCuonSach, string TenSach, int page, string tinhtrang, int madausach, int makesach)
+        {
+            string query = string.Format($"EXEC updateCuonSach '{MaCuonSach}', [{TenSach}],'{page}',[{tinhtrang}],'{madausach}','{makesach}'");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+        public bool UpdateDauSach(int MaDauSach, string TenDauSach, int MaNXB)
+        {
+            string query = string.Format($"EXEC updateDauSach '{MaDauSach}', [{TenDauSach}],'{MaNXB}'");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+        public bool UpdateTheLoai(int MaKeSach, string TenTheLoai)
+        {
+            string query = string.Format($"EXEC updateTheLoai '{MaKeSach}', [{TenTheLoai}]");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+
+
+
+
+
+
+
+    }
+}
